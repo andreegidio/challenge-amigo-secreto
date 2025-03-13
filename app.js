415d65d684cc79799b76990542891e05a991ef73
andreegidio <andreegi@gmail.com>
2025-03-13 13:58:07 -0300

    Implementação utilizando Array

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let posicaoSoteada = 0;
+let arrayListaAmigos = [];
 
 function limparCampo() {
     amigo = document.querySelector('input');
@@ -20,6 +21,9 @@ function adicionarAmigo() {
     }
     item.textContent = amigo;
     listaAmigos.appendChild(item);
+
+    arrayListaAmigos.push(amigo);
+
     limparCampo();
 }
 
@@ -37,12 +41,20 @@ function sortearAmigo() {
     let resultadoElement = document.getElementById('resultado');
     let itens = listaAmigos.children;  
 
-    posicaoSoteada = gerarPosicaoAleatorio(itens.length) - 1;
+    //Fazendo o sorteio utilizando a própria lista do HTML
+    //posicaoSoteada = gerarPosicaoAleatorio(itens.length) - 1;
+
+    //Fazendo o sorteio utilizando o array do JavaScript
+    posicaoSoteada = gerarPosicaoAleatorio(arrayListaAmigos.length) - 1;
 
     console.log('Sorteio Final:');   
     console.log(posicaoSoteada);
 
-    let resultado = itens[posicaoSoteada].textContent;
+    //Fazendo o sorteio utilizando a própria lista do HTML
+    //let resultado = itens[posicaoSoteada].textContent;
+
+    //Fazendo o sorteio utilizando o array do JavaScript
+    let resultado = arrayListaAmigos[posicaoSoteada];
 
     resultadoElement.textContent = resultado;
 
